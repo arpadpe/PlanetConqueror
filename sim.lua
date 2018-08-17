2093c7085a5fc052b8f1fcf3ceda03bb6bac3ccb
Arpad Perenyi <arpad.perenyi@gmail.com>
2018-08-17 15:21:08 +0200

    latest sim file

--- sim.lua
+++ sim.lua
@@ -2,27 +2,27 @@ agents={
     "/home/arpad/Documents/MAS/PlanetConqueror/base.lua", 1,
 }
 param = {
-     {1},        -- Parameter '1', is a static parameter which is accessible by the agents.
-     {1,1},    -- Parameter '2', going from 1 to 100, with step increments of 1.
-     {100},         -- C
-     {5},           -- D
-     {150},         -- E
+     {1},           -- Parameter '1', is a static parameter which is accessible by the agents.
+     {1},           -- Parameter '2', going from 1 to 100, with step increments of 1.
+     {50},          -- C
+     {10},          -- D
+     {200},         -- E
      {200},         -- G
      {20},          -- I
-     {0},           -- M
-     {1},           -- N
-     {8},           -- P
-     {1},           -- Q
-     {3},           -- S
-     {100},         -- T
-     {6},           -- W
-     {2},           -- X
-     {3},           -- Y
+     {1},           -- M
+     {3},           -- N
+     {5},           -- P
+     {2},           -- Q
+     {9},           -- S
+     {80},          -- T
+     {5},           -- W
+     {5},           -- X
+     {5},           -- Y
 }
 sim={
      eDistPrecision = 0.000001,
      stepPrecision  = 0.001,
-     runTime        = 500,
+     runTime        = 100,
      mapWidth       = 200,
      mapHeight      = 200,
      mapScale       = 1.0,
